0fc9ec4f55ed7187ccadf6a64098c9be1efc97e0
Matt Hunzinger <matthunz2@gmail.com>
2024-02-23 22:35:31 -0500

    Create the World struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    any::{Any, TypeId},
+    cell::UnsafeCell,
+    collections::HashMap,
+};
+
+#[derive(Default)]
+pub struct World {
+    states: HashMap<TypeId, Box<dyn Any>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl World {
+    pub fn query<'a, Q: Query<'a>>(&'a mut self) -> Q {
+        Q::query(&UnsafeCell::new(self))
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub trait Query<'a> {
+    fn query(world: & UnsafeCell<&'a mut World>) -> Self;
+}
+
+impl<'a, T: 'static> Query<'a> for &'a T {
+    fn query(world: &UnsafeCell<&'a mut World>) -> Self {
+        let world = unsafe { &mut *world.get() };
+        world
+            .states
+            .get(&TypeId::of::<T>())
+            .unwrap()
+            .downcast_ref()
+            .unwrap()
     }
 }
