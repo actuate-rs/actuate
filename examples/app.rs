3f13e5cd8ec047cb2b9dde3e69b08ec06a5f2a11
Matt Hunzinger <matthunz2@gmail.com>
2024-02-24 00:02:26 -0500

    Setup reactive graph

--- examples/app.rs
+++ examples/app.rs
@@ -1,8 +1,23 @@
 use actuate::Builder;
 
-fn a(_x: &i32) {}
+struct X(i32);
+
+struct Y(i32);
+
+fn a(X(x): &X, Y(y): &mut Y) {
+    dbg!(x);
+    *y += 1;
+}
+
+fn b(Y(y): &Y) {
+    dbg!(y);
+}
 
 fn main() {
-    let diagram = Builder::default().add_input(0).add_system(a).build();
+    let diagram = Builder::default()
+        .add_input(X(0))
+        .add_system(a)
+        .add_system(b)
+        .build();
     dbg!(diagram);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,44 @@ impl<'a, T: 'static> Query<'a> for &'a T {
     }
 }
 
+impl<'a, T: 'static> Query<'a> for &'a mut T {
+    fn reads(_ids: &mut Vec<Id>) {}
+
+    fn writes(ids: &mut Vec<Id>) {
+        ids.push(Id {
+            type_id: TypeId::of::<T>(),
+            name: any::type_name::<T>(),
+        })
+    }
+
+    fn query(world: &UnsafeCell<&'a mut World>) -> Self {
+        let world = unsafe { &mut *world.get() };
+        world
+            .states
+            .get_mut(&TypeId::of::<T>())
+            .unwrap()
+            .downcast_mut()
+            .unwrap()
+    }
+}
+
+impl<'a, Q1: Query<'a>, Q2: Query<'a>> Query<'a> for (Q1, Q2) {
+    fn reads(ids: &mut Vec<Id>) {
+        Q1::reads(ids);
+        Q2::reads(ids)
+    }
+
+    fn writes(ids: &mut Vec<Id>) {
+        Q1::writes(ids);
+        Q2::writes(ids)
+    }
+
+    fn query(world: &UnsafeCell<&'a mut World>) -> Self {
+        // TODO: check for overlaps
+        (Q1::query(world), Q2::query(world))
+    }
+}
+
 pub trait System<'a>: 'static {
     type Query: Query<'a>;
 
@@ -58,7 +96,7 @@ pub struct FnSystem<F, Marker> {
     _marker: PhantomData<Marker>,
 }
 
-impl<'a, F, Q> System<'a> for FnSystem<F, Q>
+impl<'a, F, Q> System<'a> for FnSystem<F, (Q,)>
 where
     F: Fn(Q) + 'static,
     Q: Query<'a> + 'static,
@@ -70,18 +108,47 @@ where
     }
 }
 
+impl<'a, F, Q1, Q2> System<'a> for FnSystem<F, (Q1, Q2)>
+where
+    F: Fn(Q1, Q2) + 'static,
+    Q1: Query<'a> + 'static,
+    Q2: Query<'a> + 'static,
+{
+    type Query = (Q1, Q2);
+
+    fn run(&self, query: Self::Query) {
+        (self.f)(query.0, query.1)
+    }
+}
+
 pub trait IntoSystem<'a, Marker> {
     type System: System<'a>;
 
     fn into_system(self) -> Self::System;
 }
 
-impl<'a, F, Q> IntoSystem<'a, Q> for F
+impl<'a, F, Q> IntoSystem<'a, (Q,)> for F
 where
     F: Fn(Q) + 'static,
     Q: Query<'a> + 'static,
 {
-    type System = FnSystem<F, Q>;
+    type System = FnSystem<F, (Q,)>;
+
+    fn into_system(self) -> Self::System {
+        FnSystem {
+            f: self,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'a, F, Q1, Q2> IntoSystem<'a, (Q1, Q2)> for F
+where
+    F: Fn(Q1, Q2) + 'static,
+    Q1: Query<'a> + 'static,
+    Q2: Query<'a> + 'static,
+{
+    type System = FnSystem<F, (Q1, Q2)>;
 
     fn into_system(self) -> Self::System {
         FnSystem {
@@ -115,10 +182,16 @@ impl<'a, S: System<'a>> AnySystem for S {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Id {
     type_id: TypeId,
     name: &'static str,
+}
+
+impl fmt::Debug for Id {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("Id").field(&self.name).finish()
+    }
 }
 
 #[derive(Default)]
@@ -176,7 +249,7 @@ impl Builder {
                 system.reads_any(&mut reads);
 
                 let mut writes = Vec::new();
-                system.reads_any(&mut writes);
+                system.writes_any(&mut writes);
 
                 let node = NodeData {
                     reads,
@@ -213,6 +286,8 @@ impl Builder {
                         }
                     }
                 }
+
+                queue.extend(node.writes.iter().copied());
                 nodes.insert(
                     id,
                     Node {
@@ -243,6 +318,26 @@ struct Node {
     children: Vec<Id>,
 }
 
+struct NodeChildrenDebugger<'a> {
+    node: &'a Node,
+    diagram: &'a Diagram,
+}
+
+impl fmt::Debug for NodeChildrenDebugger<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut children = f.debug_list();
+        for child_id in &self.node.children {
+            let child = self.diagram.nodes.get(child_id).unwrap();
+            children.entry(&NodeDebugger {
+                id: *child_id,
+                node: child,
+                diagram: self.diagram,
+            });
+        }
+        children.finish()
+    }
+}
+
 struct NodeDebugger<'a> {
     id: Id,
     node: &'a Node,
@@ -251,22 +346,17 @@ struct NodeDebugger<'a> {
 
 impl fmt::Debug for NodeDebugger<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let children = {
-            let mut children = f.debug_list();
-            for child_id in &self.node.children {
-                let child = self.diagram.nodes.get(child_id).unwrap();
-                children.entry(&NodeDebugger {
-                    id: *child_id,
-                    node: child,
-                    diagram: self.diagram,
-                });
-            }
-            children.finish()?
-        };
-
         let mut s = f.debug_struct(self.id.name);
-        s.field("children", &children);
-        s.finish()
+        s.field(
+            "children",
+            &NodeChildrenDebugger {
+                node: self.node,
+                diagram: self.diagram,
+            },
+        )
+        .field("reads", &self.node.data.reads)
+        .field("writes", &self.node.data.writes)
+        .finish()
     }
 }
 
