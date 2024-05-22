da4380f477f8241f0a25f7d8c116628a324b97b9
Matt Hunzinger <matthunz2@gmail.com>
2024-05-22 19:01:13 -0400

    Try out pointer approach

--- examples/app.rs
+++ examples/app.rs
@@ -1,39 +1,13 @@
-use actuate::{use_state, Scope, View, VirtualDom};
-
-struct Counter {
-    start: i32,
-}
-
-impl View for Counter {
-    fn body(&self, cx: &Scope) -> impl View {
-        let (count, set_count) = use_state(cx, || self.start);
-
-        set_count.set(count + 1);
-
-        dbg!(count);
-    }
-}
+use actuate::View;
 
 struct App;
 
 impl View for App {
-    fn body(&self, _cx: &Scope) -> impl View {
-        (
-            Counter { start: 0 },
-            Counter { start: 100 },
-            Counter { start: 200 },
-        )
+    fn body(&self) -> impl View {
+        dbg!("Hello world!");
     }
 }
 
-#[tokio::main]
-async fn main() {
-    let mut vdom = VirtualDom::new(App.into_node());
-
-    tokio::spawn(async move {
-        vdom.run().await;
-        vdom.run().await;
-    })
-    .await
-    .unwrap();
+fn main() {
+    actuate::run(App)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,89 @@
-mod scope;
-pub use self::scope::Scope;
+use slotmap::{DefaultKey, SlotMap};
+use std::{marker::PhantomData, mem};
 
-mod use_context;
-pub use self::use_context::use_context;
+pub trait View: 'static {
+    fn body(&self) -> impl View;
 
-mod use_provider;
-pub use self::use_provider::use_provider;
+    fn into_node(self) -> impl Node
+    where
+        Self: Sized,
+    {
+        ViewNode {
+            view: self,
+            body_fn: |me: &'static Self| me.body().into_node(),
+            _marker: PhantomData,
+        }
+    }
+}
 
-mod use_state;
-pub use self::use_state::{use_state, Setter};
+impl View for () {
+    fn body(&self) -> impl View {}
 
-mod vdom;
-pub use self::vdom::VirtualDom;
+    fn into_node(self) -> impl Node
+    where
+        Self: Sized,
+    {
+    }
+}
 
-pub mod view;
-pub use self::view::View;
+#[derive(Default)]
+pub struct Tree {
+    nodes: SlotMap<DefaultKey, *const dyn AnyNode>,
+}
 
-pub mod node;
-pub use self::node::Node;
+pub trait AnyNode {}
+
+impl<T: Node> AnyNode for T {}
+
+pub trait Node: 'static {
+    type State;
+
+    fn build(&self) -> Self::State;
+
+    fn init(&self, tree: &mut Tree, state: &mut Self::State);
+}
+
+impl Node for () {
+    type State = ();
+
+    fn build(&self) -> Self::State {}
+
+    fn init(&self, tree: &mut Tree, state: &mut Self::State) {}
+}
+
+pub struct ViewNode<V, F, B> {
+    view: V,
+    body_fn: F,
+    _marker: PhantomData<fn() -> B>,
+}
+
+impl<V, F, B> Node for ViewNode<V, F, B>
+where
+    V: View,
+    F: Fn(&'static V) -> B + 'static,
+    B: Node,
+{
+    type State = (B, B::State, Option<DefaultKey>);
+
+    fn build(&self) -> Self::State {
+        let view = unsafe { mem::transmute(&self.view) };
+        let body = (self.body_fn)(view);
+        let body_state = body.build();
+        (body, body_state, None)
+    }
+
+    fn init(&self, tree: &mut Tree, state: &mut Self::State) {
+        let key = tree.nodes.insert(self as _);
+        state.2 = Some(key);
+
+        state.0.init(tree, &mut state.1);
+    }
+}
+
+pub fn run(view: impl View) {
+    let mut tree = Tree::default();
+
+    let node = view.into_node();
+    let mut state = node.build();
+    node.init(&mut tree, &mut state);
+}
