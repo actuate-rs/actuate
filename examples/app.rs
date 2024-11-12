650c1f023b9c9c2d482ef04d06d2d32fd231763e
Matt Hunzinger <matt@hunzinger.me>
2024-11-12 14:15:17 -0500

    Remove `Node` trait (#15)
    * Create new Compose trait
    
    * Create AnyCompose trait
    
    * Impl Compose for Box<dyn AnyCompose>
    
    * Create DynCompose struct
    
    * Create Data trait
    
    * Impl Compose and Data for tuples
    
    * Impl Compose for &C
    
    * Update macro
    
    * Simplify Data trait
    
    * Add context hooks
    
    * Add use_memo hook

--- examples/app.rs
+++ examples/app.rs
@@ -1,48 +1,16 @@
-use actuate::{use_mut, Compose, Data, Memo, Mut, Ref, Scope};
-use std::ops::Deref;
-
-#[derive(Hash)]
-struct Button<'a, T> {
-    label: T,
-    count: Mut<'a, i32>,
-}
-
-unsafe impl<T> Data for Button<'_, T> {}
-
-impl<T> Compose for Button<'_, T>
-where
-    T: Deref<Target = str>,
-{
-    fn compose(cx: Scope<Self>) -> impl Compose {
-        dbg!(&*cx.me().label);
-
-        cx.me().count.update(|x| *x += 1)
-    }
-}
+use actuate::{Compose, Composer, Scope};
+use actuate_macros::Data;
 
 #[derive(Data)]
-struct Counter {
-    label: String,
-    initial: i32,
-}
+struct App;
 
-impl Compose for Counter {
+impl Compose for App {
     fn compose(cx: Scope<Self>) -> impl Compose {
-        let count = use_mut(&cx, || cx.me().initial);
-
-        dbg!(*count);
-
-        let label = Ref::map(cx.me(), |me| &*me.label);
-
-        (Memo::new(Button { label, count }), Button { count, label })
+        dbg!("app!");
     }
 }
 
-#[tokio::main]
-async fn main() {
-    actuate::run(Counter {
-        initial: 0,
-        label: String::from("foo"),
-    })
-    .await;
+fn main() {
+    let mut composer = Composer::new(App);
+    composer.compose();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,15 @@ use std::{
     any::{Any, TypeId},
     cell::{Cell, RefCell, UnsafeCell},
     collections::HashMap,
-    hash::{Hash, Hasher},
+    hash::{DefaultHasher, Hash, Hasher},
+    marker::PhantomData,
     mem,
     ops::Deref,
     rc::Rc,
 };
-
 use tokio::sync::mpsc;
 
-pub mod compose;
-pub use self::compose::{AnyCompose, Compose, Data, DataField, Memo, StateField};
-use self::compose::{Node, RebuildContext};
-
-pub use actuate_macros::Data;
-
+/// A mapped immutable reference to a value of type `T`.
 pub struct Map<'a, T: ?Sized> {
     ptr: *const (),
     map_fn: *const (),
@@ -48,11 +43,13 @@ impl<T: Hash + ?Sized> Hash for Map<'_, T> {
     }
 }
 
-pub struct Ref<'a, T> {
+/// An immutable reference to a value of type `T`.
+pub struct Ref<'a, T: ?Sized> {
     value: &'a T,
 }
 
 impl<'a, T> Ref<'a, T> {
+    /// Map this reference to a value of type `U`.
     pub fn map<U: ?Sized>(self, f: fn(&T) -> &U) -> Map<'a, U> {
         Map {
             ptr: self.value as *const _ as _,
@@ -65,19 +62,11 @@ impl<'a, T> Ref<'a, T> {
     }
 }
 
-impl<T> Clone for Ref<'_, T> {
-    fn clone(&self) -> Self {
-        Self { value: self.value }
-    }
-}
-
-impl<T> Copy for Ref<'_, T> {}
-
-impl<'a, T> Deref for Ref<'a, T> {
+impl<T: ?Sized> Deref for Ref<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &self.value
+        self.value
     }
 }
 
@@ -139,8 +128,43 @@ impl<'a, T> Deref for Mut<'a, T> {
     }
 }
 
+struct Update {
+    f: Box<dyn FnMut()>,
+}
+
+#[derive(Clone)]
+pub struct Runtime {
+    tx: mpsc::UnboundedSender<Update>,
+}
+
+impl Runtime {
+    pub fn current() -> Self {
+        RUNTIME.with(|runtime| {
+            runtime
+                .borrow()
+                .as_ref()
+                .expect("Runtime::current() called outside of a runtime")
+                .clone()
+        })
+    }
+
+    pub fn enter(&self) {
+        RUNTIME.with(|runtime| {
+            *runtime.borrow_mut() = Some(self.clone());
+        });
+    }
+
+    pub fn update(&self, f: impl FnMut() + 'static) {
+        self.tx.send(Update { f: Box::new(f) }).unwrap();
+    }
+}
+
+thread_local! {
+    static RUNTIME: RefCell<Option<Runtime>> = RefCell::new(None);
+}
+
 #[derive(Clone, Default)]
-pub struct Contexts {
+struct Contexts {
     values: HashMap<TypeId, Rc<dyn Any>>,
 }
 
@@ -149,14 +173,50 @@ pub struct ScopeState {
     hooks: UnsafeCell<Vec<Box<dyn Any>>>,
     hook_idx: Cell<usize>,
     is_changed: Cell<bool>,
+    is_empty: Cell<bool>,
     contexts: RefCell<Contexts>,
 }
 
-pub fn use_ref<T: 'static>(scope: &ScopeState, make_value: impl FnOnce() -> T) -> &T {
-    let hooks = unsafe { &mut *scope.hooks.get() };
+/// Composable scope.
+pub struct Scope<'a, C: ?Sized> {
+    me: &'a C,
+    state: &'a ScopeState,
+}
 
-    let idx = scope.hook_idx.get();
-    scope.hook_idx.set(idx + 1);
+impl<'a, C> Scope<'a, C> {
+    pub fn me(&self) -> Ref<'a, C> {
+        Ref { value: self.me }
+    }
+
+    pub fn state(&self) -> &'a ScopeState {
+        self.state
+    }
+}
+
+impl<C> Clone for Scope<'_, C> {
+    fn clone(&self) -> Self {
+        Self {
+            me: self.me,
+            state: self.state,
+        }
+    }
+}
+
+impl<C> Copy for Scope<'_, C> {}
+
+impl<'a, C> Deref for Scope<'a, C> {
+    type Target = &'a ScopeState;
+
+    fn deref(&self) -> &Self::Target {
+        &self.state
+    }
+}
+
+pub fn use_ref<T: 'static>(cx: &ScopeState, make_value: impl FnOnce() -> T) -> &T {
+    let hooks = unsafe { &mut *cx.hooks.get() };
+
+    let idx = cx.hook_idx.get();
+    cx.hook_idx.set(idx + 1);
 
     let any = if idx >= hooks.len() {
         hooks.push(Box::new(make_value()));
@@ -214,149 +274,310 @@ pub fn use_provider<T: 'static>(scope: &ScopeState, make_value: impl FnOnce() ->
     (*r).clone()
 }
 
-struct UseDrop {
-    f: Box<dyn FnMut()>,
-}
-
-impl Drop for UseDrop {
-    fn drop(&mut self) {
-        (self.f)()
-    }
-}
-
-pub fn use_drop<'a>(scope: &'a ScopeState, f: impl FnOnce() + 'a) {
-    let mut f_cell = Some(f);
-
-    let f: Box<dyn FnMut() + 'a> = Box::new(move || {
-        f_cell.take().unwrap()();
-    });
-    let f: Box<dyn FnMut() + 'static> = unsafe { mem::transmute(f) };
-
-    use_ref(scope, move || UseDrop { f });
-}
-
 pub fn use_memo<D, T>(scope: &ScopeState, dependency: D, make_value: impl FnOnce() -> T) -> Ref<T>
 where
-    D: PartialEq + 'static,
+    D: Hash,
     T: 'static,
 {
+    let mut hasher = DefaultHasher::new();
+    dependency.hash(&mut hasher);
+    let hash = hasher.finish();
+
     let mut make_value_cell = Some(make_value);
     let value_mut = use_mut(scope, || make_value_cell.take().unwrap()());
 
-    let mut dependency_cell = Some(dependency);
-    let dependency_mut = use_mut(scope, || dependency_cell.take().unwrap());
+    let hash_mut = use_mut(scope, || hash);
 
-    if let Some(dependency) = dependency_cell {
-        if *dependency_mut != dependency {
-            let value = make_value_cell.take().unwrap()();
+    if let Some(make_value) = make_value_cell {
+        if hash != *hash_mut {
+            let value = make_value();
             value_mut.with(move |update| *update = value);
+
+            hash_mut.with(move |dst| *dst = hash);
         }
     }
 
     value_mut.as_ref()
 }
 
-pub struct Scope<'a, C> {
-    me: &'a C,
-    state: &'a ScopeState,
+pub unsafe trait Data {
+    type Id: 'static;
 }
 
-impl<'a, C> Scope<'a, C> {
-    pub fn me(&self) -> Ref<'a, C> {
-        Ref { value: self.me }
-    }
+unsafe impl Data for () {
+    type Id = ();
+}
 
-    pub fn state(&self) -> &'a ScopeState {
-        self.state
+unsafe impl<T: ?Sized + Data> Data for &T {
+    type Id = PhantomData<&'static T::Id>;
+}
+
+unsafe impl<T: Data + ?Sized> Data for Ref<'_, T> {
+    type Id = PhantomData<Ref<'static, T::Id>>;
+}
+
+unsafe impl Data for DynCompose<'_> {
+    type Id = PhantomData<DynCompose<'static>>;
+}
+
+#[doc(hidden)]
+pub unsafe trait StateField {
+    fn check(&self) {
+        let _ = self;
     }
 }
 
-impl<C> Clone for Scope<'_, C> {
-    fn clone(&self) -> Self {
+unsafe impl<T: 'static> StateField for &T {}
+
+unsafe impl<T: 'static> StateField for Mut<'_, T> {}
+
+#[doc(hidden)]
+pub unsafe trait DataField {
+    fn check(&self) {
+        let _ = self;
+    }
+}
+
+unsafe impl<T: Data> DataField for &&T {}
+
+pub trait Compose: Data {
+    fn compose(cx: Scope<Self>) -> impl Compose;
+}
+
+impl Compose for () {
+    fn compose(cx: Scope<Self>) -> impl Compose {
+        cx.is_empty.set(true);
+    }
+}
+
+impl<C: Compose> Compose for &C {
+    fn compose(cx: Scope<Self>) -> impl Compose {
+        cx.me().any_compose(&cx);
+    }
+}
+
+pub struct DynCompose<'a> {
+    compose: UnsafeCell<Option<Box<dyn AnyCompose + 'a>>>,
+}
+
+impl<'a> DynCompose<'a> {
+    pub fn new(content: impl Compose + 'a) -> Self {
         Self {
-            me: self.me,
-            state: self.state,
+            compose: UnsafeCell::new(Some(Box::new(content))),
         }
     }
 }
 
-impl<C> Copy for Scope<'_, C> {}
+struct DynComposeState {
+    compose: Box<dyn AnyCompose>,
+    data_id: TypeId,
+}
 
-impl<'a, C> Deref for Scope<'a, C> {
-    type Target = &'a ScopeState;
+impl<'a> Compose for DynCompose<'a> {
+    fn compose(cx: Scope<Self>) -> impl Compose {
+        let cell: &UnsafeCell<Option<DynComposeState>> = use_ref(&cx, || UnsafeCell::new(None));
+        let cell = unsafe { &mut *cell.get() };
 
-    fn deref(&self) -> &Self::Target {
-        &self.state
+        let compose = unsafe { &mut *cx.me().compose.get() }.take().unwrap();
+        let compose: Box<dyn AnyCompose> = unsafe { mem::transmute(compose) };
+
+        if let Some(state) = cell {
+            if state.data_id != compose.data_id() {
+                todo!()
+            }
+
+            unsafe { *(&mut *(state.compose.as_ptr_mut() as *mut _)) = compose }
+        } else {
+            *cell = Some(DynComposeState {
+                data_id: compose.data_id(),
+                compose,
+            });
+        }
+
+        cell.as_mut().unwrap().compose.any_compose(cx.state);
     }
 }
 
-struct Update {
-    f: Box<dyn FnMut()>,
+macro_rules! impl_tuples {
+    ($($t:tt : $idx:tt),*) => {
+        unsafe impl<$($t: Data),*> Data for ($($t,)*) {
+            type Id = ($($t::Id,)*);
+        }
+
+        impl<$($t: Compose),*> Compose for ($($t,)*) {
+            fn compose(cx: Scope<Self>) -> impl Compose {
+                $(cx.me().$idx.any_compose(use_ref(&cx, || ScopeState {
+                    contexts: cx.contexts.clone(), ..Default::default()
+                }));)*
+            }
+        }
+
+    };
 }
 
-#[derive(Clone)]
-pub struct Runtime {
-    tx: mpsc::UnboundedSender<Update>,
+impl_tuples!(T1:0);
+impl_tuples!(T1:0, T2:1);
+impl_tuples!(T1:0, T2:1, T3:2);
+impl_tuples!(T1:0, T2:1, T3:2, T4:3);
+impl_tuples!(T1:0, T2:1, T3:2, T4:3, T5:4);
+impl_tuples!(T1:0, T2:1, T3:2, T4:3, T5:4, T6:5);
+impl_tuples!(T1:0, T2:1, T3:2, T4:3, T5:4, T6:5, T7:6);
+impl_tuples!(T1:0, T2:1, T3:2, T4:3, T5:4, T6:5, T7:6, T8:7);
+
+trait AnyCompose {
+    fn data_id(&self) -> TypeId;
+
+    fn as_ptr_mut(&mut self) -> *mut ();
+
+    fn any_compose<'a>(&'a self, state: &'a ScopeState);
 }
 
-impl Runtime {
-    pub fn current() -> Self {
-        RUNTIME.with(|runtime| {
-            runtime
-                .borrow()
-                .as_ref()
-                .expect("Runtime::current() called outside of a runtime")
-                .clone()
-        })
+impl<C> AnyCompose for C
+where
+    C: Compose + Data,
+{
+    fn data_id(&self) -> TypeId {
+        TypeId::of::<C::Id>()
     }
 
-    pub fn enter(&self) {
-        RUNTIME.with(|runtime| {
-            *runtime.borrow_mut() = Some(self.clone());
+    fn as_ptr_mut(&mut self) -> *mut () {
+        self as *mut Self as *mut ()
+    }
+
+    fn any_compose<'a>(&'a self, state: &'a ScopeState) {
+        let cx = Scope { me: self, state };
+
+        let cell: &UnsafeCell<Option<Box<dyn AnyCompose>>> = use_ref(&cx, || UnsafeCell::new(None));
+        let cell = unsafe { &mut *cell.get() };
+
+        let child = C::compose(cx);
+        unsafe {
+            if let Some(ref mut content) = cell {
+                *(&mut *(content.as_ptr_mut() as *mut _)) = child
+            } else {
+                let boxed: Box<dyn AnyCompose> = Box::new(child);
+                *cell = Some(mem::transmute(boxed));
+            }
+        }
+
+        if cx.state.is_empty.get() {
+            cx.state.is_empty.set(false);
+            return;
+        }
+
+        let child_state = use_ref(&cx, || ScopeState {
+            contexts: state.contexts.clone(),
+            ..Default::default()
         });
+        let child = cell.as_mut().unwrap();
+        (**child).any_compose(child_state);
     }
-
-    pub fn update(&self, f: impl FnMut() + 'static) {
-        self.tx.send(Update { f: Box::new(f) }).unwrap();
-    }
-}
-
-thread_local! {
-    static RUNTIME: RefCell<Option<Runtime>> = RefCell::new(None);
 }
 
 pub struct Composer {
+    compose: Box<dyn AnyCompose>,
+    scope_state: ScopeState,
     rt: Runtime,
     rx: mpsc::UnboundedReceiver<Update>,
 }
 
 impl Composer {
-    pub fn new() -> Self {
+    pub fn new(content: impl Compose + 'static) -> Self {
         let (tx, rx) = mpsc::unbounded_channel();
         Self {
+            compose: Box::new(content),
+            scope_state: ScopeState::default(),
             rt: Runtime { tx },
             rx,
         }
     }
 
-    pub async fn run(&mut self, compose: impl Compose) {
+    pub fn compose(&mut self) {
         self.rt.enter();
 
-        let node = compose.into_node();
-        let mut state = node.build(&Contexts::default());
+        self.compose.any_compose(&Scope {
+            me: &self.compose,
+            state: &self.scope_state,
+        });
 
-        while let Some(mut update) = self.rx.recv().await {
+        while let Ok(mut update) = self.rx.try_recv() {
             (update.f)();
-
-            while let Ok(mut update) = self.rx.try_recv() {
-                (update.f)();
-            }
-
-            node.rebuild(&mut state, &RebuildContext { is_changed: false });
         }
     }
 }
 
-pub async fn run(compose: impl Compose) {
-    Composer::new().run(compose).await;
+#[cfg(test)]
+mod tests {
+    use crate::{Compose, Composer, Data, DynCompose};
+    use std::{cell::Cell, rc::Rc};
+
+    struct Counter {
+        x: Rc<Cell<i32>>,
+    }
+
+    unsafe impl Data for Counter {
+        type Id = Self;
+    }
+
+    impl Compose for Counter {
+        fn compose(cx: crate::Scope<Self>) -> impl Compose {
+            cx.me().x.set(cx.me().x.get() + 1);
+        }
+    }
+
+    #[test]
+    fn it_works() {
+        struct Wrap {
+            x: Rc<Cell<i32>>,
+        }
+
+        unsafe impl Data for Wrap {
+            type Id = Self;
+        }
+
+        impl Compose for Wrap {
+            fn compose(cx: crate::Scope<Self>) -> impl Compose {
+                Counter {
+                    x: cx.me().x.clone(),
+                }
+            }
+        }
+
+        let x = Rc::new(Cell::new(0));
+        let mut composer = Composer::new(Wrap { x: x.clone() });
+
+        composer.compose();
+        assert_eq!(x.get(), 1);
+
+        composer.compose();
+        assert_eq!(x.get(), 2);
+    }
+
+    #[test]
+    fn it_composes_any_compose() {
+        struct Wrap {
+            x: Rc<Cell<i32>>,
+        }
+
+        unsafe impl Data for Wrap {
+            type Id = Self;
+        }
+
+        impl Compose for Wrap {
+            fn compose(cx: crate::Scope<Self>) -> impl Compose {
+                DynCompose::new(Counter {
+                    x: cx.me().x.clone(),
+                })
+            }
+        }
+
+        let x = Rc::new(Cell::new(0));
+        let mut composer = Composer::new(Wrap { x: x.clone() });
+
+        composer.compose();
+        assert_eq!(x.get(), 1);
+
+        composer.compose();
+        assert_eq!(x.get(), 2);
+    }
 }
