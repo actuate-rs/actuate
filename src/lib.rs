cfef2383fe64c4247f4c1df3d3b5466fc30b1814
Matt Hunzinger <matthunz2@gmail.com>
2024-11-17 22:46:42 -0500

    Add example to root docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,41 @@
 //! # Actuate
 //! Actuate is a native, declarative, and friendly user-interface (UI) framework.
 //!
+//! ```no_run
+//! use actuate::prelude::*;
+//!
+//! #[derive(Data)]
+//! struct Counter {
+//!     start: i32,
+//! }
+//!
+//! impl Compose for Counter {
+//!     fn compose(cx: Scope<Self>) -> impl Compose {
+//!         let count = use_mut(&cx, || cx.me().start);
+//!
+//!         Window::new((
+//!             Text::new(format!("High five count: {}", *count))
+//!                 .font(GenericFamily::Cursive)
+//!                 .font_size(60.),
+//!             Text::new("Up high")
+//!                 .on_click(move || count.update(|x| *x += 1))
+//!                 .background_color(Color::BLUE),
+//!             Text::new("Down low")
+//!                 .on_click(move || count.update(|x| *x -= 1))
+//!                 .background_color(Color::RED),
+//!             if *count == 0 {
+//!                 Some(Text::new("Gimme five!"))
+//!             } else {
+//!                 None
+//!             },
+//!         ))
+//!         .font_size(40.)
+//!     }
+//! }
+//!
+//! actuate::run(Counter { start: 0 })
+//! ```
+//!
 //! ## Hooks
 //! Functions that begin with `use_` are called `hooks` in Actuate.
 //! Hooks are used to manage state and side effects in composables.
