04673773582b5ded9a404d9655b0bd3ccfa04da0
Matt Hunzinger <matthunz2@gmail.com>
2024-11-25 16:51:41 -0500

    Add basic trait object example

---
+++ examples/traits.rs
@@ -0,0 +1,39 @@
+use actuate::prelude::*;
+
+#[data]
+trait MyTrait: Data {
+    fn run(&self);
+}
+
+#[derive(Data)]
+struct A<'a> {
+    my_trait: Box<dyn MyTrait + 'a>,
+}
+
+impl Compose for A<'_> {
+    fn compose(cx: Scope<Self>) -> impl Compose {
+        cx.me().my_trait.run();
+    }
+}
+
+#[derive(Data)]
+struct X;
+
+impl MyTrait for X {
+    fn run(&self) {
+        dbg!("X");
+    }
+}
+
+#[derive(Data)]
+struct App;
+
+impl Compose for App {
+    fn compose(_cx: Scope<Self>) -> impl Compose {
+        A {
+            my_trait: Box::new(X),
+        }
+    }
+}
+
+fn main() {}
