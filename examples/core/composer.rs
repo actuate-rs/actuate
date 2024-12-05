fb04e56ec3d885e49fbfe195f0c6a2f8a485a534
Matt Hunzinger <matthunz2@gmail.com>
2024-12-04 21:10:54 -0500

    fix: composer example

--- examples/core/composer.rs
+++ examples/core/composer.rs
@@ -36,10 +36,7 @@ fn main() {
 
     let mut composer = Composer::new(App);
 
-    // Compose each composable.
-    for _ in 0..3 {
-        composer.try_compose().unwrap();
-    }
+    composer.try_compose().unwrap();
 
     assert_eq!(composer.try_compose(), Err(TryComposeError::Pending));
 
